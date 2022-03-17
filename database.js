ee33173767e57735d7ac70073a82b6bb1855eac7
Gabriel Ibarra <toro270294@gmail.com>
2022-03-17 07:59:46 -0600

    Update database.js

--- database.js
+++ database.js
@@ -1,8 +1,8 @@
 let config = {
-	host: "localhost",
-	user: "root",
-	password: "",
+	host: "us-cdbr-east-05.cleardb.net",
+	user: "b977e3d9827fdb",
+	password: "81251994",
 	port: 3306,
-	database: "adoption_system"
+	database: "heroku_8d374393b26b2d3"
 };
 exports.config = config;
